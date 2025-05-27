71e5c96b2f3cfc55a6831ec18879d2d897ef6876
Jahrin Binte Zahid <jahrinzahid@gmail.com>
2025-05-27 16:42:32 +0600

    Added some comments

--- LinearSearch/problem1.cpp
+++ LinearSearch/problem1.cpp
@@ -5,3 +5,5 @@ int main() {
     cout << "Hello, World!" << endl;
     return 0;
 }
+// This program prints "Hello, World!" to the console.
+// It serves as a basic example of a C++ program structure, including the use of the iostream library for input and output operations.
